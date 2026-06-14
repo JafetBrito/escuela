36f8b83e8781f668a5bd7b315361497ffe55d536
JafetBrito <jafetbrito028@protonmail.com>
2026-06-14 14:51:55 -0600

    meow meow

--- src/stores/useCurrencyStore.js
+++ src/stores/useCurrencyStore.js
@@ -2,8 +2,8 @@ import { create } from 'zustand'
 
 // "Monedas" earned by completing MISIONES, spendable in la Tienda.
 // Stored as "cobre" (copper); 10000 cobre = 1 oro. New accounts start with
-// 28,000 oro of seed currency.
-const STARTING_COINS = 28000 * 10000
+// 5 oro of seed currency.
+const STARTING_COINS = 5 * 10000
 
 export const useCurrencyStore = create((set, get) => ({
   coins: STARTING_COINS,
